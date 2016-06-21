b8a920c176e905d1ed9bd2cca9abbfe5ef9e88b1
Indriel <pa.pachernig@gmail.com>
2016-06-21 22:43:12 +0200

    Update TokenColor.java

--- android/src/com/mygdx/game/Spiel/TokenColor.java
+++ android/src/com/mygdx/game/Spiel/TokenColor.java
@@ -4,7 +4,7 @@ package com.mygdx.game.Spiel;
  * Created by Florian on 23.05.2016.
  */
 public class TokenColor {
-    public static int color;
-    public static boolean won=false;
+    public static final int color;
+    public static final boolean won=false;
 
 }
